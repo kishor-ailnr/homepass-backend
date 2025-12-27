0f095a1a7696104e53a7e99a5aaf1160d8f54247
Kishor Raj <rajkishor23849@gmail.com>
2025-12-27 08:00:44 +0530

    Fix verified sender email

--- mailer.js
+++ mailer.js
@@ -4,12 +4,13 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendMail(to, subject, text) {
   const msg = {
-    to,
-    from: "rajkishor23849@gmail.com", // VERIFIED sender only
-    replyTo: "rajkishor23849@gmail.com",
-    subject,
-    text,
-  };
+  to: "rajkishor23849@gmail.com", // test to yourself
+  from: "rajkishor23849@gmail.com",
+  replyTo: "rajkishor23849@gmail.com",
+  subject: "HomePass Test Mail",
+  text: "This is a test email from HomePass App",
+};
+
 
   try {
     await sgMail.send(msg);
